93832363ec64f85ec2c6923d027804d27c069acc
arpansharma <arpans0995@gmail.com>
2025-01-21 17:02:16 -0600

    Validation error responses

--- src/main/java/in/arpansharma/expense_tracker_api/exception/GlobalExceptionHandler.java
+++ src/main/java/in/arpansharma/expense_tracker_api/exception/GlobalExceptionHandler.java
@@ -1,17 +1,23 @@
 package in.arpansharma.expense_tracker_api.exception;
 
 import in.arpansharma.expense_tracker_api.models.ErrorObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorObject> handleExpenseNotFoundException(ResourceNotFoundException ex, WebRequest webRequest){
@@ -33,13 +39,26 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorObject> handleException(Exception ex, WebRequest webRequest){
-        ErrorObject errorObject = new ErrorObject();
-        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorObject.setMessage(ex.getMessage());
-        errorObject.setTimestamp(new Timestamp(System.currentTimeMillis()));
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<ErrorObject> handleException(Exception ex, WebRequest webRequest){
+//        ErrorObject errorObject = new ErrorObject();
+//        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+//        errorObject.setMessage(ex.getMessage());
+//        errorObject.setTimestamp(new Timestamp(System.currentTimeMillis()));
+//
+//        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
-        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.INTERNAL_SERVER_ERROR);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,Object> body= new HashMap<>();
+        body.put("timestamp",new Timestamp(System.currentTimeMillis()));
+        body.put("statusCode",HttpStatus.BAD_REQUEST.value());
+
+        String message  = ex.getBindingResult().getFieldError().getDefaultMessage();
+
+        body.put("message",message);
+
+        return new ResponseEntity<Object>(body,HttpStatus.BAD_REQUEST);
     }
 }
