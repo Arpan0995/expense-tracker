1ed063346b98608b20bfdbb4303279ce206b89d8
arpansharma <arpans0995@gmail.com>
2025-01-21 16:08:47 -0600

    Handling Bad Request

--- src/main/java/in/arpansharma/expense_tracker_api/exception/GlobalExceptionHandler.java
+++ src/main/java/in/arpansharma/expense_tracker_api/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.Timestamp;
 
@@ -20,5 +21,15 @@ public class GlobalExceptionHandler {
         errorObject.setTimestamp(new Timestamp(System.currentTimeMillis()));
 
         return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest webRequest){
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage("Id can only be a number, please fix the id parameter " + ex.getMessage());
+        errorObject.setTimestamp(new Timestamp(System.currentTimeMillis()));
+
+        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.BAD_REQUEST);
     }
 }
