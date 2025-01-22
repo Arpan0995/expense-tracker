622ba125a1f0544116b138d12a003278f5722161
arpansharma <arpans0995@gmail.com>
2025-01-21 22:42:46 -0600

    Swagger tags for all endpoints

--- src/main/java/in/arpansharma/expense_tracker_api/controller/ExpenseController.java
+++ src/main/java/in/arpansharma/expense_tracker_api/controller/ExpenseController.java
@@ -28,43 +28,58 @@ public class ExpenseController {
     }
 
     @Operation(summary = "All Expenses")
+    @ApiResponse(responseCode = "200",description = "List of All Expenses")
     @GetMapping("/allExpenses")
     public List<Expense> fetchAllExpenses(Pageable page){
         return expService.getExpenses(page).toList();
     }
 
+    @Operation(summary = "Expense Info")
+    @ApiResponse(responseCode = "200",description = "Expense Info")
     @GetMapping("/expenses/{id}")
     public Expense fetchExpenseById(@PathVariable Long id){
         return expService.getExpenseById(id);
     }
 
+    @Operation(summary = "Delete an expense")
+    @ApiResponse(responseCode = "200",description = "Deleting an expense")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/delete")
     public void deleteExpense(@RequestParam("id") Long id){
         expService.deleteExpense(id);
     }
 
+    @Operation(summary = "Adding an Expense")
+    @ApiResponse(responseCode = "200",description = "Adds an expense")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/addExpense")
     public Expense addExpense(@Valid @RequestBody Expense expense){
         return expService.insertExpense(expense);
     }
 
+    @Operation(summary = "Updating an Expense")
+    @ApiResponse(responseCode = "200",description = "Updates an expense")
     @PutMapping("/updateExpense/{id}")
     public Expense updateExpense(@RequestBody Expense expense, @PathVariable Long id){
         return expService.updateExpense(id, expense);
     }
 
+    @Operation(summary = "Filter by Expense")
+    @ApiResponse(responseCode = "200",description = "Filter by expense")
     @GetMapping("/expenses/category")
     public List<Expense> getByCategory(@RequestParam String category, Pageable page){
         return expService.getByCategory(category,page).stream().toList();
     }
 
+    @Operation(summary = "Filter by Name")
+    @ApiResponse(responseCode = "200",description = "Filter by name")
     @GetMapping("/expenses/name")
     public List<Expense> getByName(@RequestParam String name, Pageable page){
         return expService.getByName(name, page).stream().toList();
     }
 
+    @Operation(summary = "Filter by Date")
+    @ApiResponse(responseCode = "200",description = "Filter by date")
     @GetMapping("/expenses/date")
     public List<Expense> getByDateRange(@RequestParam(required = false) Date startDate,@RequestParam(required = false) Date endDate, Pageable page){
         return expService.getByDateRange(startDate,endDate,page).stream().toList();
