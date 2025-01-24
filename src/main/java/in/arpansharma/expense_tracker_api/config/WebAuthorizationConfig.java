f7176ae02965ae74a9aefdb426c892076670ef4a
arpansharma <arpans0995@gmail.com>
2025-01-24 01:09:09 -0600

    Add user login

--- src/main/java/in/arpansharma/expense_tracker_api/config/WebAuthorizationConfig.java
+++ src/main/java/in/arpansharma/expense_tracker_api/config/WebAuthorizationConfig.java
@@ -3,8 +3,11 @@ package in.arpansharma.expense_tracker_api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,4 +23,10 @@ public class WebAuthorizationConfig {
 
         return httpSecurity.build();
     }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+       return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
--- src/main/java/in/arpansharma/expense_tracker_api/controller/AuthController.java
+++ src/main/java/in/arpansharma/expense_tracker_api/controller/AuthController.java
@@ -1,5 +1,6 @@
 package in.arpansharma.expense_tracker_api.controller;
 
+import in.arpansharma.expense_tracker_api.models.LoginReq;
 import in.arpansharma.expense_tracker_api.models.User;
 import in.arpansharma.expense_tracker_api.models.UserModel;
 import in.arpansharma.expense_tracker_api.service.UserService;
@@ -7,24 +8,37 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(){
-        return new ResponseEntity<String>("User logged in",HttpStatus.OK);
+    public ResponseEntity<HttpStatus> login(@RequestBody LoginReq loginReq){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.getEmail()
+        ,loginReq.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
     }
 
     @PostMapping("/register")
     public ResponseEntity<User> registerUser(@Valid @RequestBody UserModel userModel){
         return new ResponseEntity<User>(userService.registerUser(userModel),HttpStatus.CREATED);
     }
+
 }
